10f6f6c7bba7992627e5edce25e9920c9e99f439
EneaSema <sema.enea1993@gmail.com>
2025-07-25 12:58:53 +0200

    finish-snack2

--- main.js
+++ main.js
@@ -35,3 +35,14 @@ console.log("la somma di num3 + num4 è:" + newSumNumbers(num3, num4));
 const arrowSumNumbers = (num1, num3) => num1 + num3;
 
 console.log("la somma di num1 + num3 è:" + arrowSumNumbers(num1, num3));
+
+/* 🏆 Snack 2
+Crea una arrow function che calcola il quadrato di un numero.
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+*/
+
+function square(num1) {
+  return num1 * num1;
+}
+
+console.log("il quadrato del numero selezionato è:" + square(num1));
