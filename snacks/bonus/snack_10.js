f80a291c913dfcbb35299195c193308c80b87271
EneaSema <sema.enea1993@gmail.com>
2025-07-30 23:08:18 +0200

    working-snack10

---
+++ snacks/bonus/snack_10.js
@@ -0,0 +1,15 @@
+/* BONUS
+🎯 Snack 10 (Bonus)
+Creare un throttler per limitare l’esecuzione di una funzione
+Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
+
+Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
+*/
+
+console.log("Esercizio 10");
+
+function stampDone() {
+  console.log("Eseguito");
+}
+
+const limite = 3000;
--- snacks/bonus/snack_9.js
+++ snacks/bonus/snack_9.js
@@ -1,4 +1,5 @@
-/* Snack 9 (Bonus)
+/* BONUS
+🎯 Snack 9 (Bonus)
 Creare una funzione che esegue una sequenza di operazioni con ritardi
 Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
 
@@ -10,33 +11,33 @@ console.log("Esercizio 9");
 const a = 6;
 const b = 9;
 
-function somma(a, b) {
-  return a + b;
-}
-function minus(a, b) {
-  return b - a;
-}
-function moltiplication(a, b) {
-  return a * b;
-}
-const intervallo = 1000;
+// function somma(a, b) {
+//   return a + b;
+// }
+// function minus(a, b) {
+//   return b - a;
+// }
+// function moltiplication(a, b) {
+//   return a * b;
+// }
+// const intervallo = 1000;
 
-const stopTime = 3000;
+// const stopTime = 3000;
 
-function sequenzaOperazioni([somma, minus, moltiplication], intervallo) {
-  return setInterval(function () {
-    console.log(somma);
-    console.log(minus);
-    console.log(moltiplication);
-  }, intervallo);
-}
+// function sequenzaOperazioni([somma, minus, moltiplication], intervallo) {
+//   return setInterval(function () {
+//     console.log(somma);
+//     console.log(minus);
+//     console.log(moltiplication);
+//   }, intervallo);
+// }
 
-const sequenza = sequenzaOperazioni(
-  [somma(a, b), minus(a, b), moltiplication(a, b)],
-  intervallo
-);
+// const sequenza = sequenzaOperazioni(
+//   [somma(a, b), minus(a, b), moltiplication(a, b)],
+//   intervallo
+// );
 
-setTimeout(function () {
-  clearInterval(sequenza);
-  console.log("fine");
-}, stopTime);
+// setTimeout(function () {
+//   clearInterval(sequenza);
+//   console.log("fine");
+// }, stopTime);
